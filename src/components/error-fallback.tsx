1aa36b3d4468b56452086df0abb81bf7c98ae815
koralle <koralle+github@koralle-mgmg.com>
2026-07-22 00:31:33 +0900

    feat(ui): add icons to shared state components

--- src/components/error-fallback.tsx
+++ src/components/error-fallback.tsx
@@ -1,8 +1,13 @@
+import { TriangleAlert } from 'lucide-react'
 import { FallbackProps, getErrorMessage } from 'react-error-boundary'
 
 export function ErrorFallback({ error }: FallbackProps) {
   return (
     <div role='alert'>
+      <TriangleAlert
+        size={20}
+        aria-hidden
+      />
       <p>{getErrorMessage(error)}</p>
     </div>
   )
