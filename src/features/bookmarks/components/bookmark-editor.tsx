0095423f7e5f3853f486c0126b4a4c5ea3147f25
koralle <koralle+github@koralle-mgmg.com>
2026-08-02 02:04:18 +0900

    style(bookmarks): format editor imports

--- src/features/bookmarks/components/bookmark-editor.tsx
+++ src/features/bookmarks/components/bookmark-editor.tsx
@@ -4,8 +4,8 @@ import type { CreateTag, SelectableTag } from '../../tags/application/create-tag
 import type { TagId } from '../../tags/domain/tag-values'
 import type { ExecuteUpdateBookmark } from '../application/execute-update-bookmark'
 import type { BookmarkEditorData } from '../application/load-bookmark-for-edit'
-import { recoverSelectableTagsPromise } from '../application/load-selectable-tags';
-import type { LoadSelectableTags, SelectableTagsResult } from '../application/load-selectable-tags';
+import { recoverSelectableTagsPromise } from '../application/load-selectable-tags'
+import type { LoadSelectableTags, SelectableTagsResult } from '../application/load-selectable-tags'
 import type { BookmarkId } from '../domain/bookmark-values'
 import { mapUpdateBookmarkError } from './bookmark-editor-error'
 import { BookmarkForm } from './bookmark-form'
