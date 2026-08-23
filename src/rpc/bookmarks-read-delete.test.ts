8ef9e4f507bf715562fc520e2688af5fe4db0f71
koralle <koralle+github@koralle-mgmg.com>
2026-08-24 04:04:13 +0900

    style: avoid variable shadowing in bookmarks RPC test

--- src/rpc/bookmarks-read-delete.test.ts
+++ src/rpc/bookmarks-read-delete.test.ts
@@ -62,12 +62,12 @@ function createTestClient(router: AppRouter, headers?: HeadersInit) {
 }
 
 async function rejection(promise: Promise<unknown>): Promise<ORPCError<string, unknown>> {
-  const error = await promise.then(
+  const rejected = await promise.then(
     () => null,
     (error: unknown) => error
   )
-  expect(error).toBeInstanceOf(ORPCError)
-  return error as ORPCError<string, unknown>
+  expect(rejected).toBeInstanceOf(ORPCError)
+  return rejected as ORPCError<string, unknown>
 }
 
 describe('bookmarks RPC 契約', () => {
